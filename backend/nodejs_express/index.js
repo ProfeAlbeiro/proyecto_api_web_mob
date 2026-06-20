590647a27fdaa51a0645f5e1241a0fd7242b6428
ProfeAlbeiro <profealbeiro2020@gmail.com>
2026-06-20 14:46:08 -0500

    .env de ejemplo

--- backend/nodejs_express/index.js
+++ backend/nodejs_express/index.js
@@ -2,12 +2,12 @@ const http = require('http');
 const app = require('./server');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || '10.1.196.46';
+const host = process.env.HOST || '192.168.56.1';
 
 // Configuración CORS
 app.use(cors({
   origin: [
-    'http://10.1.196.46',   // IP de tu frontend
+    'http://192.168.56.1',   // IP de tu frontend
     'http://localhost:5173',   // Desarrollo local
     'http://localhost:3000',   
     'http://127.0.0.1:5173',
